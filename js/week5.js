79f165680cc0cf60c423e71cafec8cf319c392da
michellejames <shellyjames90@yahoo.com>
2017-05-01 14:57:48 -0400

    Twitter API

--- js/week5.js
+++ js/week5.js
@@ -1,23 +1,26 @@
 
 var TwitterApi = (function(options) {
 	var shared = {};
-	var options = options || {};
 
 	function processTimelineResults(results){
 
 		var $apiResults = JSON.parse(results);
-		console.log($apiResults);
+		// console.log($apiResults);
 
 		for (var i = 0; i < $apiResults.length; i++) {
 			var timelineQuery = document.querySelector(".timeline-query");
 
 			var screenName = document.createElement("div");
 			$(screenName).html("@"+$apiResults[i].user.screen_name).addClass("screen-name");
+
+			var tweetDate = document.createElement("div");
+			$(tweetDate).html($apiResults[i].created_at).addClass("date");
 			
 			var tweetText = document.createElement("div");
 			$(tweetText).html($apiResults[i].text).addClass("tweet");
 
 			timelineQuery.appendChild(screenName);
+			timelineQuery.appendChild(tweetDate);
 			timelineQuery.appendChild(tweetText);
 		}
 	}
@@ -25,38 +28,87 @@ var TwitterApi = (function(options) {
 	function processHashtagResults(results) {
 
 		var $apiResults = JSON.parse(results);
-		console.log($apiResults.search_metadata.query);
-		console.log($apiResults.statuses[0].user.screen_name);
-		console.log($apiResults.statuses[0].user.text);
+		// console.log($apiResults);
 
-		for (var i = 0; i < $apiResults.length; i++) {
+		for (var i = 0; i < $apiResults.statuses.length; i++) {
 
 			var hashtagQuery = document.querySelector(".hashtag-query");
 
 			var searchQuery = document.createElement("div");
-			$(searchQuery).html("Query: "+$apiResults[i].search_metadata.query).addClass("hashtag");
+			$(searchQuery).html("Query: " + $apiResults.search_metadata.query).addClass("hashtag");
 
 			var screenName = document.createElement("div");
 			$(screenName).html("@"+$apiResults.statuses[i].user.screen_name).addClass("screen-name");
 
-			var hashtagTweet = document.createElement("div");
-			$(hashtagTweet).html($apiResults[i].text);
+			var tweetDate = document.createElement("div");
+			$(tweetDate).html($apiResults.statuses[i].created_at).addClass("date");
 
-			hashtagQuery.appendChild(screenName);
-			hashtagQuery.appendChild(hashtagTweet);
+			var hashtagTweet = document.createElement("div");
+			$(hashtagTweet).html($apiResults.statuses[i].text).addClass("tweet");
+
 			hashtagQuery.appendChild(searchQuery);
+			hashtagQuery.appendChild(screenName);
+			hashtagQuery.appendChild(tweetDate);
+			hashtagQuery.appendChild(hashtagTweet);
+
 		}
 	}
 
-	var deleteResults = function () {
+	function processCustomResults (results) {
+		var $apiResults = JSON.parse(results);
+		console.log($apiResults);
+
+
+		for (var i = 0; i < $apiResults.statuses.length; i++) {
+
+			var customQuery = document.querySelector(".custom-query");
+
+			var searchQuery = document.createElement("div");
+			$(searchQuery).html("Query: " + $apiResults.search_metadata.query).addClass("hashtag");
+
+			var screenName = document.createElement("div");
+			$(screenName).html("@"+$apiResults.statuses[i].user.screen_name).addClass("screen-name");
+
+			var tweetDate = document.createElement("div");
+			$(tweetDate).html($apiResults.statuses[i].created_at).addClass("date");
+
+			var hashtagTweet = document.createElement("div");
+			$(hashtagTweet).html($apiResults.statuses[i].text).addClass("tweet");
+
+			customQuery.appendChild(searchQuery);
+			customQuery.appendChild(screenName);
+			customQuery.appendChild(tweetDate);
+			customQuery.appendChild(hashtagTweet);
+		}
+	}
+
+	var deleteTimelineResults = function () {
 
 		var $timelineQuery = $(".timeline-query");
         var $screenName = $(".screen-name");
+        var $tweetDate = $(".date");
         var $tweetText = $(".tweet");
 
         for (var i = 0; i < $screenName.length; i++) {
           $screenName[i].parentNode.removeChild($screenName[i]);
+          $tweetDate[i].parentNode.removeChild($tweetDate[i]);
           $tweetText[i].parentNode.removeChild($tweetText[i]);
+        }
+    }
+
+	var deleteHashtagResults = function () {
+
+		var $timelineQuery = $(".timeline-query");
+        var $screenName = $(".screen-name");
+        var $tweetDate = $(".date");
+        var $tweetText = $(".tweet");
+        var $hashtag = $(".hashtag");
+
+        for (var i = 0; i < $screenName.length; i++) {
+          $screenName[i].parentNode.removeChild($screenName[i]);
+          $tweetDate[i].parentNode.removeChild($tweetDate[i]);
+          $tweetText[i].parentNode.removeChild($tweetText[i]);
+          $hashtag[i].parentNode.removeChild($hashtag[i]);
         }
     }
 
@@ -64,16 +116,23 @@ var TwitterApi = (function(options) {
 
 		$("#submit-button").on("click", function (e) {
 		    e.preventDefault();
-		    deleteResults();
+		    deleteTimelineResults();
 			$.ajax("twitter-proxy.php?op=user_timeline&screen_name=" + $('.timeline-search-field').val())
 			.done(processTimelineResults)
 		});
 
 		$("#submit-hashtag-button").on("click", function (e) {
 		    e.preventDefault();
-		    deleteResults();
+		    deleteHashtagResults();
 			$.ajax("twitter-proxy.php?op=search_tweets&q=" + $('.hashtag-search-field').val())
 			.done(processHashtagResults)
+		});
+
+		$("#submit-custom-search-button").on("click", function (e) {
+		    e.preventDefault();
+		    deleteHashtagResults();
+			$.ajax("twitter-proxy.php?op=search_tweets&q=" + $('.custom-search-field').val()+"&count=" + $('.count-search-field').val()+"&result_type=" + $('.result-type-field').val())
+			.done(processCustomResults)
 		});
 	};
 
