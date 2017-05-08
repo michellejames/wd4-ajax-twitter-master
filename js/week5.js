a626bff6363570361d7fec3f12889e4a11ceb098
michellejames <shellyjames90@yahoo.com>
2017-05-08 16:49:37 -0400

    Adding Regex

--- js/week5.js
+++ js/week5.js
@@ -2,22 +2,64 @@
 var TwitterApi = (function(options) {
 	var shared = {};
 
+	function urlHighlight (text) {
+		var urlRegex = /((http|ftp|https)?:\/\/\S+)/g;
+		//console.log(urlRegex); //regex call
+		var urlRegexCall = text.match(urlRegex); //calling regex on imported data
+		//console.log(urlRegexCall); //result of regex call
+		var defaultURL = text; //tweet itself, default value in case we don't have a link
+		//console.log(defaultURL);
+		if (urlRegexCall) {
+			for (var i = 0; i < urlRegexCall.length; i++) {
+			 defaultURL = text.replace(urlRegex, "<a href='"+ urlRegexCall[i] + "' target='_blank' style='color:white;'>" + urlRegexCall[i] + "</a>");
+			}
+		}
+		return defaultURL;
+
+	}
+
+	function hashtagHighlight (text) {
+		var hashtagRegex = /(\S*#\[[^\]]+\])|(\S*#\S+)/gi;
+		var hashtagRegexCall = text.match(hashtagRegex);
+		var defaultHashtag = text;
+		if (hashtagRegexCall) {
+			for (var i = 0; i < hashtagRegexCall.length; i++) {
+			 defaultHashtag = text.replace(hashtagRegex, "<a href='https://twitter.com/search?src=typd&q"+ hashtagRegexCall[i] + "' target='_blank' style='color:white;'>" + hashtagRegexCall[i] + "</a>");
+			}
+		}
+		return defaultHashtag;
+
+	}
+
+	function usernameHighlight (text) {
+		var usernameRegex = /(\S*@\[[^\]]+\])|(\S*@\S+)/gi;
+		var usernameRegexCall = text.match(usernameRegex);
+		var defaultUsername = text;
+		if (usernameRegexCall) {
+			for (var i = 0; i < usernameRegexCall.length; i++) {
+			 defaultUsername = text.replace(usernameRegex, "<a href='https://twitter.com/"+ usernameRegexCall[i] + "' target='_blank' style='color:white;'>" + usernameRegexCall[i] + "</a>");
+			}
+		}
+		return defaultUsername;
+
+	}
+
 	function processTimelineResults(results){
 
 		var $apiResults = JSON.parse(results);
-		// console.log($apiResults);
+		console.log($apiResults);
 
 		for (var i = 0; i < $apiResults.length; i++) {
 			var timelineQuery = document.querySelector(".timeline-query");
 
 			var screenName = document.createElement("div");
-			$(screenName).html("@"+$apiResults[i].user.screen_name).addClass("screen-name");
+			$(screenName).html("<a href='" + "@" +$apiResults[i].user.screen_name).addClass("screen-name");
 
 			var tweetDate = document.createElement("div");
 			$(tweetDate).html($apiResults[i].created_at).addClass("date");
 			
 			var tweetText = document.createElement("div");
-			$(tweetText).html($apiResults[i].text).addClass("tweet");
+			$(tweetText).html(usernameHighlight(hashtagHighlight(urlHighlight($apiResults[i].text)))).addClass("tweet");
 
 			timelineQuery.appendChild(screenName);
 			timelineQuery.appendChild(tweetDate);
@@ -28,29 +70,28 @@ var TwitterApi = (function(options) {
 	function processHashtagResults(results) {
 
 		var $apiResults = JSON.parse(results);
-		// console.log($apiResults);
+		console.log($apiResults.search_metadata.query);
 
 		for (var i = 0; i < $apiResults.statuses.length; i++) {
 
 			var hashtagQuery = document.querySelector(".hashtag-query");
 
 			var searchQuery = document.createElement("div");
-			$(searchQuery).html("Query: " + $apiResults.search_metadata.query).addClass("hashtag");
+			$(searchQuery).html("Query: " + "<span style='color:red;'>" + $apiResults.search_metadata.query + "</span>").addClass("hashtag");
 
 			var screenName = document.createElement("div");
-			$(screenName).html("@"+$apiResults.statuses[i].user.screen_name).addClass("screen-name");
-
+			$(screenName).html("<a href='https://twitter.com/"+ $apiResults.statuses[i].user.screen_name + "' target='_blank' style='color:#55ACEE;'>" + "@" +$apiResults.statuses[i].user.screen_name + "</a>").addClass("screen-name");
+			
 			var tweetDate = document.createElement("div");
 			$(tweetDate).html($apiResults.statuses[i].created_at).addClass("date");
 
 			var hashtagTweet = document.createElement("div");
-			$(hashtagTweet).html($apiResults.statuses[i].text).addClass("tweet");
+			$(hashtagTweet).html(usernameHighlight(hashtagHighlight(urlHighlight($apiResults.statuses[i].text)))).addClass("tweet");
 
 			hashtagQuery.appendChild(searchQuery);
 			hashtagQuery.appendChild(screenName);
 			hashtagQuery.appendChild(tweetDate);
 			hashtagQuery.appendChild(hashtagTweet);
-
 		}
 	}
 
@@ -67,13 +108,13 @@ var TwitterApi = (function(options) {
 			$(searchQuery).html("Query: " + $apiResults.search_metadata.query).addClass("hashtag");
 
 			var screenName = document.createElement("div");
-			$(screenName).html("@"+$apiResults.statuses[i].user.screen_name).addClass("screen-name");
-
+			$(screenName).html("<a href='https://twitter.com/"+ $apiResults.statuses[i].user.screen_name + "' target='_blank' style='color:#55ACEE;'>" + "@" +$apiResults.statuses[i].user.screen_name + "</a>").addClass("screen-name");
+			
 			var tweetDate = document.createElement("div");
 			$(tweetDate).html($apiResults.statuses[i].created_at).addClass("date");
 
 			var hashtagTweet = document.createElement("div");
-			$(hashtagTweet).html($apiResults.statuses[i].text).addClass("tweet");
+			$(hashtagTweet).html(usernameHighlight(hashtagHighlight(urlHighlight($apiResults.statuses[i].text)))).addClass("tweet");
 
 			customQuery.appendChild(searchQuery);
 			customQuery.appendChild(screenName);
